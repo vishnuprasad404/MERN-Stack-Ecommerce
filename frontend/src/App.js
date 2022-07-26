dc1b7db397398a3decdf53a93865479b0c2e3586
vishnuprasad404 <vishnuprasadn2000@gmail.com>
2022-07-26 07:46:33 -0400

    admin login page updated and keep login session

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import SigninPage from "./Pages/SigninPage/SigninPage";
 import SignupPage from "./Pages/SignupPage/SignupPage";
@@ -8,7 +8,7 @@ import ProductsPage from "./Pages/ProductsPage/ProductsPage";
 import ViewProductPage from "./Pages/ViewProductPage/ViewProductPage";
 import CartPage from "./Pages/CartPage/CartPage";
 import OrdersPage from "./Pages/OrdersPage/OrdersPage";
-import FavoritesPage from './Pages/FavoritesPage/FavoritesPage'
+import FavoritesPage from "./Pages/FavoritesPage/FavoritesPage";
 import AccountPage from "./Pages/AccountPage/AccountPage";
 import AdminPage from "./Pages/AdminPage/AdminPage";
 import AdminLoginPage from "./Pages/AdminLoginPage/AdminLoginPage";
@@ -16,7 +16,7 @@ import AdminAddProduct from "./Components/AdminManageProduct/AdminManageProduct"
 import AdminDashboard from "./Components/AdminDashboard/AdminDashboard";
 import AdminViewOrders from "./Components/AdminViewOrders/AdminViewOrders";
 import AdminViewProducts from "./Components/AdminViewProducts/AdminViewProducts";
-import AdminViewUsers from './Components/AdminViewUsers/AdminViewUsers'
+import AdminViewUsers from "./Components/AdminViewUsers/AdminViewUsers";
 import OrderProductPage from "./Pages/OrderProductPage/OrderProductPage";
 import DeliveryAddressPage from "./Pages/DeliveryAddressPage/DeliveryAddressPage";
 import { useEffect, useState } from "react";
@@ -26,46 +26,83 @@ import OrderPlacedNotifyPage from "./Pages/OrderPlacedNotifyPage/OrderPlacedNoti
 import OrdersItemDetailPage from "./Pages/OrdersItemDetailPage/OrdersItemDetailPage";
 
 function App() {
-  
+  const nav = useNavigate()
+  const [user, setUser] = useState(false);
+  const [admin, setAdmin] = useState(false);
 
-  let admin = true
-  const [user, setUser] = useState(true)
-  axios.defaults.withCredentials = true
-  useEffect(()=>{
-      axios.get(`${process.env.REACT_APP_BASE_URL}/user`).then((res)=>{
-        setUser(res.data)
-      })
-  })
+  axios.defaults.withCredentials = true;
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BASE_URL}/admin`).then((res) => {
+      console.log(res.data);
+      setAdmin(res.data.isLoggedIn);
+    });
+    axios.get(`${process.env.REACT_APP_BASE_URL}/user`).then((res) => {
+      setUser(res.data);
+    });
+  });
   return (
     <div className="App">
-    <GlobalData.Provider value={{user,setUser}}>
-      <Routes>
-        <Route path="/admin-login" element={admin ? <AdminPage/> :<AdminLoginPage />} />
-        <Route path="/admin" element={admin ? <AdminPage /> : <AdminLoginPage/>} >
-          <Route path="" element={<AdminDashboard/>}/>
-          <Route path="orders" element={<AdminViewOrders/>}/>
-          <Route path="products" element={<AdminViewProducts/>}/>
-          <Route path="product/:action" element={<AdminAddProduct/>}/>
-          <Route path="users" element={<AdminViewUsers/>}/>
-        </Route>
+      <GlobalData.Provider value={{ user, setUser }}>
+        <Routes>
+          <Route
+            path="/admin-login"
+            element={admin ? <AdminPage /> : <AdminLoginPage />}
+          />
+          <Route
+            path="/admin"
+            element={admin ? <AdminPage /> : <AdminLoginPage />}
+          >
+            <Route path="" element={<AdminDashboard />} />
+            <Route path="orders" element={<AdminViewOrders />} />
+            <Route path="products" element={<AdminViewProducts />} />
+            <Route path="product/:action" element={<AdminAddProduct />} />
+            <Route path="users" element={<AdminViewUsers />} />
+          </Route>
           <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={!user ? <SignupPage /> : <HomePage/>} />
-          <Route path="/signin" element={!user ? <SigninPage /> : <HomePage/>} />
+          <Route
+            path="/signup"
+            element={!user ? <SignupPage /> : <HomePage />}
+          />
+          <Route
+            path="/signin"
+            element={!user ? <SigninPage /> : <HomePage />}
+          />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/product/:id" element={<ViewProductPage />} />
-          <Route path="/cart" element={user ? <CartPage /> : <SigninPage/>} />
-          <Route path="/orders" element={user ? <OrdersPage /> : <SigninPage/>} />
-          <Route path="/view-order-product/:id" element={user ? <OrdersItemDetailPage /> : <SigninPage/>} />
-          <Route path="/favorites" element={user ? <FavoritesPage /> : <SigninPage/>} />
-          <Route path="/account" element={user ? <AccountPage /> : <SigninPage/>} />
-          <Route path="/delivery-address" element={user ? <DeliveryAddressPage /> : <SigninPage/>} />
-          <Route path="/checkout/:OrderId" element={user ? <OrderProductPage /> : <SigninPage/>} />
-          <Route path="/order-placed-successfully/:OrderId" element={user ? <OrderPlacedNotifyPage /> : <SigninPage/>} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+          <Route path="/cart" element={user ? <CartPage /> : <SigninPage />} />
+          <Route
+            path="/orders"
+            element={user ? <OrdersPage /> : <SigninPage />}
+          />
+          <Route
+            path="/view-order-product/:id"
+            element={user ? <OrdersItemDetailPage /> : <SigninPage />}
+          />
+          <Route
+            path="/favorites"
+            element={user ? <FavoritesPage /> : <SigninPage />}
+          />
+          <Route
+            path="/account"
+            element={user ? <AccountPage /> : <SigninPage />}
+          />
+          <Route
+            path="/delivery-address"
+            element={user ? <DeliveryAddressPage /> : <SigninPage />}
+          />
+          <Route
+            path="/checkout/:OrderId"
+            element={user ? <OrderProductPage /> : <SigninPage />}
+          />
+          <Route
+            path="/order-placed-successfully/:OrderId"
+            element={user ? <OrderPlacedNotifyPage /> : <SigninPage />}
+          />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </GlobalData.Provider>
     </div>
   );
 }
- 
+
 export default App;
--- frontend/src/Pages/AdminLoginPage/AdminLoginPage.jsx
+++ frontend/src/Pages/AdminLoginPage/AdminLoginPage.jsx
@@ -1,15 +1,60 @@
-import React from 'react'
-import './AdminLoginPage.css'
+import React from "react";
+import "./AdminLoginPage.css";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import {Loading} from '../../Components/Loading/Loading'
+import { useState } from "react";
 
 function AdminLoginPage() {
+  const nav = useNavigate()
+  const [loading, setLoading] = useState(false)
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onAdminLogin = (data) => {
+    setLoading(true)
+    axios.post(`${process.env.REACT_APP_BASE_URL}/admin/signin`,data).then((res)=>{
+      setLoading(false)
+      if(res.data){
+        alert('admin login success')
+        nav('/admin')
+      }else{
+        alert("admin login faild")
+      }
+    })
+  };
   return (
     <div className="admin-login-page">
-        <div className="admin-login-container">
-            <h2>Admin Login</h2>
-            <button>LOGIN</button>
-        </div>
+        <form onSubmit={handleSubmit(onAdminLogin)}>
+          <h2>Admin Login</h2>
+          <input
+            type="text"
+            placeholder="Username"
+            className="admin-login-input"
+            {...register("username", { required: true })}
+          />
+          <error className="err">
+            {errors.username?.type === "required" && "*Username is required"}
+          </error>
+          <input
+            type="password"
+            placeholder="Password"
+            className="admin-login-input"
+            {...register("password", { required: true,minLength : 8 })}
+
+          />
+          <error className="err">
+            {errors.password?.type === "required" && "*Password is required"}
+            {errors.password?.type === "minLength" &&
+              "*Password much contain atlest 8 character"}
+          </error>
+          <button type="submit" style={{background : loading ? "lightgrey" : null}}>{!loading ? 'Login' : <Loading iconSize="19px" color="grey"/>}</button>
+        </form>
     </div>
-  )
+  );
 }
 
-export default AdminLoginPage
+export default AdminLoginPage;
